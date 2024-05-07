22cb5280ee336e44a5bdc7e7df03fa319e1624bc
Helin Wang <49939413+WangHelin1997@users.noreply.github.com>
2024-05-06 23:29:27 -0400

    Update test.sh

--- s3prl/test.sh
+++ s3prl/test.sh
@@ -22,19 +22,19 @@ META_DIR=/data/lmorove1/hwang258/data/commonvoice/metadata
 python3 run_downstream.py -m test \
     -t ${META_DIR}/alldata_TODO.csv \
     -x ${META_DIR}/alldata.csv \
-    -u hubert -e ${RESULT_DIR}/downstream/DreamVoiceBrightHubertSpecAug/states-8000.ckpt
+    -u hubert -e ${RESULT_DIR}/downstream/DreamVoiceBrightHubertSpecAug/dev-best.ckpt
 
 python3 run_downstream.py -m test \
     -t ${META_DIR}/alldata.csv \
     -x ${META_DIR}/alldata.csv \
-    -u hubert -e ${RESULT_DIR}/downstream/DreamVoiceSmoothHubertSpecAug/states-8000.ckpt
+    -u hubert -e ${RESULT_DIR}/downstream/DreamVoiceSmoothHubertSpecAug/dev-best.ckpt
 
 python3 run_downstream.py -m test \
     -t ${META_DIR}/alldata.csv \
     -x ${META_DIR}/alldata.csv \
-    -u hubert -e ${RESULT_DIR}/downstream/SpeechEmotionHubert/states-30000.ckpt
+    -u hubert -e ${RESULT_DIR}/downstream/SpeechEmotionHubert/dev-best.ckpt
 
 python3 run_downstream.py -m test \
     -t ${META_DIR}/alldata.csv \
     -x ${META_DIR}/alldata.csv \
-    -u hubert -e ${RESULT_DIR}/downstream/CommonVoiceGenderHubert/states-30000.ckpt
+    -u hubert -e ${RESULT_DIR}/downstream/CommonVoiceGenderHubert/dev-best.ckpt
