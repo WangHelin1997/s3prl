ec9b4ece77b8906771f08e9481a5bbe4527d4e2c
WangHelin1997 <wanghl925799764@gmail.com>
2024-05-06 23:27:37 -0400

    First commit

---
+++ s3prl/start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+#SBATCH --time=12:00:00
+#SBATCH --partition=a100
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:1
+#SBATCH --qos=qos_gpu
+#SBATCH --account=lmorove1_gpu 
+#SBATCH --job-name=”s3prl”
+
+source activate wav2vec
+ml purge
+ml
+
+nvidia-smi
+
+python3 run_downstream.py -m train -n CommonVoiceAgeHubert -u hubert -d age -s hidden_states
+
+# python3 run_downstream.py -m train -n SpeechEmotionHubert -u hubert -d emotion -s hidden_states -l 4 -f
+
+# python3 run_downstream.py -m train -n CommonVoiceGenderHubert -u hubert -d gender -s hidden_states -l 4 -f
+
+# python3 run_downstream.py -m train -n DreamVoiceBrightHubert -u hubert -d bright -s hidden_states -l 4 -f
+
+# python3 run_downstream.py -m train -n DreamVoiceSmoothHubert -u hubert -d smooth -s hidden_states -l 4 -f
+
+# python3 run_downstream.py -m train -n CommonVoiceAgeHubertSpecAug -u hubert -d age -s hidden_states -l 4 -f
+
+# python3 run_downstream.py -m train -n CommonVoiceAgeHubertAll -u hubert -d age -s hidden_states -f
+
+# python3 run_downstream.py -m train -n SpeechEmotionHubertSpecAug -u hubert -d emotion -s hidden_states -l 4 -f
+
+# python3 run_downstream.py -m train -n DreamVoiceBrightHubertSpecAug -u hubert -d bright -s hidden_states -l 4 -f
+
+# python3 run_downstream.py -m train -n DreamVoiceSmoothHubertSpecAug -u hubert -d smooth -s hidden_states -l 4 -f
---
+++ s3prl/test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+#SBATCH --time=01:00:00
+#SBATCH --partition=a100
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:1
+#SBATCH --qos=qos_gpu
+#SBATCH --account=lmorove1_gpu 
+#SBATCH --job-name=”s3prl”
+
+source activate wav2vec
+ml purge
+ml
+
+nvidia-smi
+
+CUDA_VISIBLE_DEVICES=0
+
+RESULT_DIR=/data/lmorove1/hwang258/data/commonvoice/s3prl/s3prl/result
+META_DIR=/data/lmorove1/hwang258/data/commonvoice/metadata
+
+python3 run_downstream.py -m test \
+    -t ${META_DIR}/alldata_TODO.csv \
+    -x ${META_DIR}/alldata.csv \
+    -u hubert -e ${RESULT_DIR}/downstream/DreamVoiceBrightHubertSpecAug/states-8000.ckpt
+
+python3 run_downstream.py -m test \
+    -t ${META_DIR}/alldata.csv \
+    -x ${META_DIR}/alldata.csv \
+    -u hubert -e ${RESULT_DIR}/downstream/DreamVoiceSmoothHubertSpecAug/states-8000.ckpt
+
+python3 run_downstream.py -m test \
+    -t ${META_DIR}/alldata.csv \
+    -x ${META_DIR}/alldata.csv \
+    -u hubert -e ${RESULT_DIR}/downstream/SpeechEmotionHubert/states-30000.ckpt
+
+python3 run_downstream.py -m test \
+    -t ${META_DIR}/alldata.csv \
+    -x ${META_DIR}/alldata.csv \
+    -u hubert -e ${RESULT_DIR}/downstream/CommonVoiceGenderHubert/states-30000.ckpt
